e6211536ab505c1a0c1d36d78aa9d79c0ed4b810
Vladimir Dimitrov <pinciflucnik1@gmail.com>
2025-11-27 18:00:49 +0200

    some more css

--- exchange-calculator/src/Calculator.jsx
+++ exchange-calculator/src/Calculator.jsx
@@ -4,7 +4,7 @@ export default function Calculator() {
     const { onChange, result, isNegative } = useCalc({ total: 0, paidBGN: 0 })
 
     return (
-        <>
+        <div className="inner-wrapper">
             <div className="calc-wrapper">
                 <form action="">
                     <label htmlFor="total">Стойност в EUR</label>
@@ -14,8 +14,8 @@ export default function Calculator() {
                 </form>
             </div>
             <div className="result-wrapper">
-                {result == 0 ? <></> : <>{isNegative ? <h5>За доплащане: {result}</h5> : <h5>Ресто: {result}EUR</h5>}</>}
+                {result == 0 ? <></> : <>{isNegative ? <h3>За доплащане: {result * -1} EUR</h3> : <h3>Ресто: {result} EUR</h3>}</>}
             </div>
-        </>
+        </div>
     )
 }
